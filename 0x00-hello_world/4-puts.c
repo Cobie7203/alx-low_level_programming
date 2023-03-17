b2a666d5e6d5a675b4f0940b3c86d535fb887979
Cobie7203 <cobie7203@gmail.com>
2023-03-16 19:33:32 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this section is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzel):
+	return (0):
+}
